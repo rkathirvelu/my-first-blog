7d223b9974016a3fd9e57fe3c05a120607a19ce3
rkathirvelu <rajesh.kathirvelu@gmail.com>
2017-05-14 09:16:44 +0530

    commit settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '6&q5es$6bbxl%um@m*sjr9qrq6@w_d1qkby5*fw%9jdnd$a0dd'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: please don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
